92a181dcbc36ad35e0b62a914332a0493687226f
Marouan <marouan.boulli.simplon@gmail.com>
2022-02-15 09:26:22 +0100

    delete app

--- Appweb/Menu/admin.py
+++
@@ -1,6 +0,0 @@
-from typing import Sequence
-from django.contrib import admin
-
-from .models import pole
-
-admin.site.register(pole)
--- Appweb/Menu/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import *
-from django import forms
-
-
-class Filtreform(forms.Form):
-	Liste_intitule = [("SCIENTIST","SCIENTIST"),("ANALYST",'ANALYST'),("manager",'manager'),("ENGINEER",'ENGINEER'),
-                   ("manager",'manager')]
-
-	intitule = forms.ChoiceField(choices=Liste_intitule)
